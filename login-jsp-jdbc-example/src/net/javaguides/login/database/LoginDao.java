cc0a3b51b0def48d85718e1764d4266acab41d93
adriaanbakker_java <adriaan.bakker@hotmail.com>
2019-11-29 17:17:23 +0100

    added login example JSP with MySQL connection

---
+++ login-jsp-jdbc-example/src/net/javaguides/login/database/LoginDao.java
@@ -0,0 +1,60 @@
+package net.javaguides.login.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import net.javaguides.login.bean.LoginBean;
+
+public class LoginDao {
+
+	public boolean validate(LoginBean loginBean) throws ClassNotFoundException {
+		boolean status = false;
+
+		//Class.forName("com.mysql.jdbc.Driver");
+		//com.mysql.cj.jdbc.Driver
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		try (Connection connection = DriverManager
+				.getConnection("jdbc:mysql://localhost:3306/db_example?useSSL=false", "root", "mypassword");
+
+				
+				// Step 2:Create a statement using connection object
+				PreparedStatement preparedStatement = connection
+						.prepareStatement("select * from login where username = ? and password = ? ")) {
+			preparedStatement.setString(1, loginBean.getUsername());
+			preparedStatement.setString(2, loginBean.getPassword());
+
+			System.out.println(preparedStatement);
+			ResultSet rs = preparedStatement.executeQuery();
+			
+			status = rs.next();
+			//int count = rs.getInt("rowcount");
+			//System.out.println("rows found:" + count);
+
+			
+		} catch (SQLException e) {
+			// process sql exception
+			printSQLException(e);
+		}
+		return status;
+	}
+
+	private void printSQLException(SQLException ex) {
+		for (Throwable e : ex) {
+			if (e instanceof SQLException) {
+				e.printStackTrace(System.err);
+				System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+				System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+				System.err.println("Message: " + e.getMessage());
+				Throwable t = ex.getCause();
+				while (t != null) {
+					System.out.println("Cause: " + t);
+					t = t.getCause();
+				}
+			}
+		}
+	}
+}
